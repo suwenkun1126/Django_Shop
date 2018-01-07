53a176b3314c67fad751eea5765adfbea25a4719
suwenkun1126 <suwenkun1126@126.com>
2018-01-07 16:49:37 +0800

    创建购物车类

--- Django_Shop/settings.py
+++ Django_Shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+
+CART_SESSION_ID='cart'
