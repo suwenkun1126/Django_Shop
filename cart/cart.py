ca26cf793a0f1514fe49b21d53e26cfdba5f6348
suwenkun1126 <suwenkun1126@126.com>
2018-01-07 21:24:48 +0800

    添加购物车详情页模板

--- cart/cart.py
+++ cart/cart.py
@@ -14,7 +14,7 @@ class Cart(object):
         product_id=str(product.id)
         if product_id not in self.cart:
             self.cart[product_id]={'quantity':0,
-                                   'price':product.price}
+                                   'price':str(product.price)}
         if update_quantity:
             self.cart[product_id]['quantity']=quantity
         else:
@@ -46,7 +46,7 @@ class Cart(object):
         return sum(item['quantity'] for item in self.cart.values())
 
     def get_total_price(self):
-        return sum(Decimal(item['price']*item['quantity'] for item in self.cart.values()))
+        return sum(Decimal(item['price'])*item['quantity'] for item in self.cart.values())
 
     def clear(self):
         del self.cart[settings.CART_SESSION_ID]
