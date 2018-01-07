3544d5518274167e295cf8eb73f3ff7ad5d44b94
suwenkun1126 <suwenkun1126@126.com>
2018-01-07 22:32:25 +0800

    购物车添加可更新产品数量

--- cart/views.py
+++ cart/views.py
@@ -6,6 +6,10 @@ from django.views.decorators.http import require_POST
 
 def cart_detail(request):
     cart=Cart(request)
+    for item in cart:
+        item['update_quantity_form']=CartAddProductForm(initial={'quantity':item['quantity'],
+                                                        'update':True,}
+                                                        )
     return render(request,'cart/detail.html',{'cart':cart})
     pass
 
